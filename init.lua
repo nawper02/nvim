87f4f9ecb9247454e932fde150d8417030cf75af
Kin <kin.blandford@tealdrones.com>
2026-01-12 10:22:23 -0700

    ctrl a ctrl c

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Ctrl+A to Select All
+vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+-- Ctrl+C to Copy selection to system clipboard
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
