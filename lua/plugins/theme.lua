9156aaf9d5a22130eaa3ce38dfca404f0ed4af9e
Kin <kin.blandford@tealdrones.com>
2026-01-12 11:42:40 -0700

    Finally a good fucking colorscheme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,11 @@
 return {
   {
-    "iagorrr/noctis-high-contrast.nvim",
-    lazy = false,
-    priority = 1000,
+    "tpope/vim-vividchalk",
+    lazy = false, -- Make sure it loads immediately
+    priority = 1000, -- Make sure it loads before other plugins
     config = function()
-      -- This theme is very high contrast by default
-      vim.cmd("colorscheme noctishc")
+      -- This sets the colorscheme when the plugin loads
+      vim.cmd([[colorscheme vividchalk]])
     end,
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "noctishc",
-    },
   },
 }
