115df00ab9360ad37bd6e7262048b12d4c2eb2a1
Kin <kin.blandford@tealdrones.com>
2026-01-22 10:28:21 -0700

    lsp and no autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = false,
+  underline = false,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+      signs = false,
+      underline = false,
+    })
+  end,
+})
+
+vim.g.autoformat = false
